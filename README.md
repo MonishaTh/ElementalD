97e65efba3f320ece0bb3f38223bfb141e1ff153
Will <whoward6@gmu.edu>
2020-10-17 14:32:32 -0400

    combining elements function (requirement #3)

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # ElementalD
-To use this bot, you'll need to install some modules. This can be done with 
+To use this bot, you'll need to create a virtual environment and install some modules. To do this (linux) use the following commands:
+ virtualenv ElementalD-env
+ source ElementalD-env/bin/activate
  pip install -r requirements.txt
+
+Then to run it, you'll need to do
+ source ElementalD-env/bin/activate
--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 # bot.py
-import os
+import os, csv
 
 import discord
 from dotenv import load_dotenv
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+PREFIX = "!" #TODO: Make this not hard-coded.
 
 class CustomClient(discord.Client):
     async def on_ready(self):
@@ -19,8 +20,24 @@ class CustomClient(discord.Client):
     async def on_member_join(self,member):
         pass#For later
     async def on_message(self,message):
-        if "Hello" in message.content:
-            await message.channel.send("I'm not ready yet.")
+        content=message.content
+        channel=message.channel
+        if(content.startswith(PREFIX)):
+            args=content.split(" ")
+            command=args[0][len(PREFIX):]#remove the prefix from the command
+            #print(args)
+            if(command=="combine"):
+                with open("combinations.csv","r") as cominationsFile:
+                    combinationsreader=csv.reader(cominationsFile)
+                    for combination in combinationsreader:
+                        #print(combination)
+                        if(combination[1]==args[1] and combination[2]==args[2]):
+                            print("user "+str(message.author)+" successfully combined "+combination[1]+" with "+combination[2]+" to get "+combination[0])
+                            await channel.send(combination[1]+"+"+combination[2]+"="+combination[0])
+                            break
+                    else:
+                        print("user "+str(message.author)+" made an invalid combination")    
+                        await channel.send("Invalid combination")
 
 
 
